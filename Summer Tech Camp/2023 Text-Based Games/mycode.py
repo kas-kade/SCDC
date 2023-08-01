a061de7e7aed09167378ab3520369b81a6ac0b1b
mars <64494517+kas-kade@users.noreply.github.com>
2023-08-01 11:40:33 -0700

    Add files via upload

---
+++ Summer Tech Camp/2023 Text-Based Games/mycode.py
@@ -0,0 +1,27 @@
+
+answer=input("lets play would you rather" )
+if (answer.lower() == "ok" or answer.lower() == "yes"):
+    question1 = input("would you rather eat hamburgers or pizza? ")
+    if (question1 == "pizza"):
+        question2= input("would you rather be in DC or Marvel? ")
+        if (question2 == "DC"):
+            print("------------------------------")
+            print("|        You're done          |")
+            print("------------------------------")
+        if (question2 == "Marvel"):
+            print("------------------------------")
+            print("|  You passed the vibe test   |")
+            print("------------------------------")
+
+    elif (question1 == "hamburger"):
+        question3=input("would you rather have super strength or be indestructible? ")
+        if   (question3=="super strength"):
+            question4 = input ("would you rather choose messi or ronaldo? ")
+            if (question4 == "ronaldo"):
+                print("------------------------------")  
+                print("|        Game Over           |")
+                print("------------------------------")
+            if    (question4 == "messi"):
+                print("-------------------------------")
+                print("|     congratulations         |")
+                print("-------------------------------")
---
+++ Summer Tech Camp/2023 Text-Based Games/oh.py
@@ -0,0 +1,33 @@
+# Start Game Here 
+answer = input("would you like to do a puzzle")
+
+if(answer == "yes"):
+    answer2 = input("what puzzle would you like to do? 1. puzzle one or 2. puzzle two?")
+    if answer2 == ("2"):
+        print("in this puzzle you have to figure out which words go together")
+        answer = input("the letter options you get are SBAKHCOLOLTO")
+        if (answer.lower() == "table"):
+            print("This answer is wrong you will be logged out")
+        if (answer.lower() == "school"):
+            print("Yes you have got these right, now you have ended puzzle two")
+        if (answer.lower() == "back"):
+            print("Yes you have got these right, now you have ended puzzle two")
+        if (answer.lower() == "too"):
+            print("Yes you have got these right, now you have ended puzzle two")
+    if answer2 == ("1"):
+        print("in this puzzle you have to figure out which words go together")
+        answer = input("the letter options you get are PUFCRSSOORDWAEZZLN")
+        if (answer.lower() == "puzzle"):
+            print("Yes you have got these right, now you have ended puzzle one")
+        if (answer.lower() == "are"):
+            print("Yes you have got these right, now you have ended puzzle one")
+        if (answer.lower() == "crossword"):
+            print("Yes you have got these right, now you have ended puzzle one")
+        if (answer.lower() == "perfect"):
+            print("This answer is wrong you will be logged out")
+
+elif(answer == "no"):
+    answer2 = input("would you like to end the game")
+    if(answer2 == "yes"):
+        print("ok ending game now...")
+    
