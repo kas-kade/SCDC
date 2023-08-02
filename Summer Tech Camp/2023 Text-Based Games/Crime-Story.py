d70f25ac77987e43502b95ea0b3f5e7b3f75239f
mars <64494517+kas-kade@users.noreply.github.com>
2023-08-02 14:22:47 -0700

    Add files via upload

---
+++ Summer Tech Camp/2023 Text-Based Games/Crime-Story.py
@@ -0,0 +1,32 @@
+answer = input("You commited a crime, are you going to run away from the cops?")
+
+if (answer == "yes"):
+    print("the police were worried for the citizens saftey so they had no choice to shoot you, your now in jail")
+
+elif (answer == "no"):
+    print("you guilty consious would not alow you to run, you dummy now your in jail")
+
+else:
+    print("either way you arressted hahaha Loser")
+
+answer = input("you woke up in your jail cell, are you going to talk to your prisonmates?")
+
+if (answer == "yes"):
+    print("your prisonmates are bothered so they punch you and beat you to be quiet, and you nap in pain")
+
+elif (answer == "no"):
+    print("you prisonmates don't find you anoying and they think your cool, your tired so you take a nap")
+
+else:
+    print("you decide to take a nap and reflect on your actions")
+
+answer = input("you wake up from your nap, should you act on your best behavior?")
+
+if (answer == "yes"):
+    print("your great behavior is praised and your get released from prison")
+
+elif (answer == "no"):
+    print("you decided the prsion life is for you prisonmates respect you create a gang in jail")
+
+else:
+    print("you cry yourself to sleep hoping it was all a dream")
