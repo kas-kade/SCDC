d737ed27a437b911a8ef395bb3eda464f46c7eef
mars <64494517+luxemic@users.noreply.github.com>
2024-10-03 12:27:08 -0700

    Add files via upload

---
+++ Summer Tech Camp/2024 HTML Websites/kelani/Main.java
@@ -0,0 +1,15 @@
+
+/*Author:me
+ description: simple code that demonstrates the use of comments*/
+public class Main {
+
+    public static void main(String[] args) {
+        //dis finna print out hello wrld
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+    }
+}
